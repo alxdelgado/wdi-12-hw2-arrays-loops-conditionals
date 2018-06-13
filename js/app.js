b5d27709ee8faa12ef10c1d59637cd671789bc6d
Alex Delgado <delgadoalex@icloud.com>
2018-06-13 17:59:07 -0500

    Commit 2 - Get even answered

--- js/app.js
+++ js/app.js
@@ -3,7 +3,17 @@ console.log('JS is working');
 // Easy Going // Section 1 // 
 
 // 1. //
-for (let i = 1; i <= 20; i++){
+// for (let i = 1; i <= 20; i++){
+
+//     console.log(i);
+
+// }
+
+
+// Get Even // Section 2 // 
+
+// 1. // 
+for (let i = 0; i <= 200; i += 2){
 
     console.log(i);
 
