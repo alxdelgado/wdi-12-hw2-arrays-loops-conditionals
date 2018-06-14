5e7ba55f10357d7c973fc2b18eb607d7f3182c02
Alex Delgado <delgadoalex@icloud.com>
2018-06-13 19:18:47 -0500

    Fizz Buzz answered

--- js/app.js
+++ js/app.js
@@ -13,8 +13,82 @@ console.log('JS is working');
 // Get Even // Section 2 // 
 
 // 1. // 
-for (let i = 0; i <= 200; i += 2){
+// for (let i = 0; i <= 200; i += 2){
+
+//     console.log(i);
+
+// }
+
+// Excited Kitten // Section 3 //
+
+// 1. // 
+
+    // for (let i = 0; i <= 20; i++){
+
+    //     console.log('love me, pet me! HSSSSS!' + i);
+      
+    // if (i % 2 === 0){
+
+    //     console.log(Math.random('human... why are you taking pictures of me?'));
+        
+    //   }
+
+    // }
+
+    // for every even number un your loop, log ""
+    // "human... why are you taking pictures of me?"
+    // "the catnip made me do it!"
+    // "why does the red dot always get away"
+    // at random 
+
+    // console.log(Math.random()); 
+
+
+
+// FizzBuzz // Section 4 // 
+
+for (let i = 0; i <= 100; i++){
 
     console.log(i);
+    
+  if (i % 3 === 0){
+
+    console.log('Fizz');
+
+    }
+
+  if (i % 5 === 0){
+
+    console.log('Buzz');
+
+  }
+
+  if (i % 3 && 5 === 0){
+
+    console.log('FizzBuzz');
+
+  }
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
